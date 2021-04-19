90c5b8efa2485166aafdce2b62728815028f053f
xcs <1013826258@qq.com>
2021-04-19 11:35:15 +0800

    第一次使用idea提交代码

---
+++ BasicCode/myArrayTest/src/com/xcs/Demo/Demo1Array.java
@@ -0,0 +1,16 @@
+package com.xcs.Demo;
+
+/**
+ * @author xcs
+ * @create 2021-04-17 14:16
+ */
+public class Demo1Array {
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+        //System.out.println(arr);
+
+        char[] cArr = new char[4];
+        System.out.println(cArr);
+
+    }
+}
